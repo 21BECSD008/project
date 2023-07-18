2ca57f70a6dfa49ded5ea5f8893f9ab3cb87ba62
21becsd008 <mrabbas1688@gmail.com>
2023-07-19 00:05:36 +0530

    project added

---
+++ placement project/java.js
@@ -0,0 +1,11 @@
+let login=document.getElementById('login');
+let nam=document.getElementById('na');
+let pass=document.getElementById('pa');
+login.addEventListener('click',()=>{
+    if(nam!=null||nam!=''){
+        alert('hi');
+    }
+    else{
+        alert('enter values');
+    }
+}) 
